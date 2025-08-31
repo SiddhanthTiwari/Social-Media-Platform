77834a9da07ba7392c1d6c44028d9a7b04b2bf28
Siddharth Tiwari <siddharthtiwari5295@gmail.com>
2025-08-31 14:57:59 +0530

    frontend deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL= "http://localhost:9090"
-
+export const BASE_URL= "https://social-media-platform-tvsx.onrender.com/"
 
 export const clientServer=axios.create({
     baseURL:BASE_URL,
